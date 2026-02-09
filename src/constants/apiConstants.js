ba71d0c4d7c33fd7cf4da17dc5f8c8ab6001e157
Samuel Santos <samuel.santos@inventsoftware.com.br>
2026-02-09 08:53:39 -0300

    minor: change base_url api

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,5 +1,5 @@
 export const ApiConstants = {
     //BASE_URL: "https://localhost:7268/api",
-    BASE_URL: "https://backed-pgp-developer-connect.trycloudflare.com/api",
+    BASE_URL: "https://obtained-ads-spam-organisation.trycloudflare.com/api",
     POLLING_INTERVAL: 300000 // 5 minute in ms
 };
