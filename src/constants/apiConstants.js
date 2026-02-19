0603c47a3ac754067fd85cf119a1a0a4d8f41509
Samuel Santos <samuel.santos@inventsoftware.com.br>
2026-02-19 13:50:55 -0300

    feat: update link vm api

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,5 +1,5 @@
 export const ApiConstants = {
     //BASE_URL: "https://localhost:7268/api",
-    BASE_URL: "https://obtained-ads-spam-organisation.trycloudflare.com/api",
+    BASE_URL: "https://trainers-wright-carnival-blackberry.trycloudflare.com/api",
     POLLING_INTERVAL: 300000 // 5 minute in ms
 };
