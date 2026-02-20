cc9962bc096c012df857ddc0208f315c119f7828
Samuel Santos <samuel.santos@inventsoftware.com.br>
2026-02-20 18:19:09 -0300

    change link api

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,5 +1,5 @@
 export const ApiConstants = {
-    //BASE_URL: "https://localhost:7268/api",
-    BASE_URL: "https://trainers-wright-carnival-blackberry.trycloudflare.com/api",
+    //BASE_URL: "https://localhost:7268/api"
+    BASE_URL: "https://tied-align-roman-hughes.trycloudflare.com/api",
     POLLING_INTERVAL: 300000 // 5 minute in ms
 };
