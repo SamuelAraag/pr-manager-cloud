56267025458baf35e99b1f9bb88809045fc6b9e9
Samuel Santos de Jesus <samuel.santos@inventsoftware.com.br>
2026-02-20 07:25:32 -0300

    feat: include local dev status scream

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -2,3 +2,7 @@ export const ApiConstants = {
     //BASE_URL: "https://localhost:7268/api",
     BASE_URL: "https://trainers-wright-carnival-blackberry.trycloudflare.com/api",
 };
+
+export function isLocalDev() {
+    return ApiConstants.BASE_URL.includes('localhost') || ApiConstants.BASE_URL.includes('127.0.0.1');
+}
