613d815f1f8432355d153853f41666ae33bad9bf
Samuel Santos <samuel.santos@inventsoftware.com.br>
2026-05-19 17:59:33 -0300

    update url api

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,6 +1,6 @@
 export const ApiConstants = {
   //BASE_URL: "https://localhost:7268/api",
-  BASE_URL: "https://mouse-spray-proudly-seasons.trycloudflare.com/api",
+  BASE_URL: "https://cartridges-jake-versions-ministers.trycloudflare.com/api",
 };
 
 export function isLocalDev() {
